1ba678ee771bbb6e35bba9f7baeeeea2a3d2df56
kderev <killia@dereviere.fr>
2025-02-17 14:39:16 +0100

    .

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  const newarray = array.slice();
+  const index = newarray.indexOf(args);
+  newarray.splice(index, 1);
+
+  return newarray;
 };
 
 // Do not edit below this line
