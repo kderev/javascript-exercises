0b02d952b201ba8fbd8db82c62ad19cc47fea189
kderev <killia@dereviere.fr>
2025-02-17 15:05:05 +0100

    check if leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  const isDivisibleby4 = Number.isInteger(year / 4);
+  const isDivisibledby100 = Number.isInteger(year / 100);
+  const isDivisibleby400 = Number.isInteger(year / 400);
+  if (isDivisibleby4) {
+    if (isDivisibledby100) {
+      return isDivisibleby400;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
