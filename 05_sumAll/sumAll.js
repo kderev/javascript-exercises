cc3b807fd021d18f89ab18e76054aedad1603ca6
kderev <killia@dereviere.fr>
2025-02-17 14:38:48 +0100

    sumAll + checking if int

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, lastNumber) {
+  let sum = 0;
+  let temp = 0;
 
+  if (firstNumber < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(firstNumber)) {
+    return "ERROR";
+  } else if (!Number.isInteger(lastNumber)) {
+    return "ERROR";
+  }
+
+  if (firstNumber > lastNumber) {
+    temp = firstNumber;
+    firstNumber = lastNumber;
+    lastNumber = temp;
+  }
+  for (i = firstNumber; i <= lastNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
