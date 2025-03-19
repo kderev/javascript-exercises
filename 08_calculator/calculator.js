5a4e71847238c4ee2ed1ac7daa999d32eb8c359a
kderev <killian@dereviere.fr>
2025-03-19 15:28:36 +0100

    complete js exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function (term1, term2) {
+  let result = term1 + term2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (term1, term2) {
+  let result = term1 - term2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  let result = 0;
+  for (i = 0; i < num.length; i++) {
+    result += num[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (product) {
+  let result = 1;
+  for (i = 0; i < product.length; i++) {
+    result *= product[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  let result = 1;
+
+  for (i = 0; i < power; i++) {
+    result *= number;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 0;
+  if (n === 0) {
+    return 1;
+  } else {
+    result = n * factorial(n - 1);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +52,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
