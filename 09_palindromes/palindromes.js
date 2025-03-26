3f959f4267059beffefdf5b8a44c15b062004ac6
kderev <killian@dereviere.fr>
2025-03-26 12:58:27 +0100

    complete js exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let bool = true;
+  let firstCharCompared = (string = string.replace(/[^a-zA-Z0-9]/g, ""));
 
+  let secondCharCompared = (string = string.replace(/[^a-zA-Z0-9]/g, ""));
+
+  string = string.replace("!" || "," || "." || "?", "");
+  string = string.toLowerCase();
+
+  console.log(firstCharCompared);
+  console.log(secondCharCompared);
+
+  for (i = 0; i < string.length; i++) {
+    firstCharCompared = string.charAt(i);
+
+    secondCharCompared = string.charAt(string.length - 1 - i);
+
+    if (firstCharCompared != secondCharCompared) {
+      bool = false;
+    }
+  }
+  return bool;
 };
 
 // Do not edit below this line
