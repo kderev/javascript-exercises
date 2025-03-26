103b6a6351e24e6a20103545e0512315ef74af18
kderev <killian@dereviere.fr>
2025-03-26 14:04:33 +0100

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  number = parseInt(number);
+  console.log(number);
+  if (number < 0) {
+    return "OOPS";
+  }
+  if (number === 0) {
+    return 0;
+  }
+  let temp = 0;
+  let a = 0;
+  let b = 1;
+  for (i = 1; i < number; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 };
 
 // Do not edit below this line
