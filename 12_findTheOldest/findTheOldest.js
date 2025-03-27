e7622c3d0cff0383083cd17086993a2d1fd44c5c
kderev <killian@dereviere.fr>
2025-03-27 15:47:26 +0100

    complete an array exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = 2025;
+  let oldestPerson = people[0];
 
+  for (let i = 1; i < people.length; i++) {
+    const person = people[i];
+
+    const deathYear = person.yearOfDeath || currentYear;
+    const age = deathYear - person.yearOfBirth;
+
+    const oldestDeathYear = oldestPerson.yearOfDeath || currentYear;
+    const oldestAge = oldestDeathYear - oldestPerson.yearOfBirth;
+
+    if (age > oldestAge) {
+      oldestPerson = person;
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
